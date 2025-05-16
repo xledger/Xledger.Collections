192ce9d47735490fd50c4c014910464a8202a1ec
Matthew O'Connor <matthew.oconnor@xledger.com>
2025-05-16 09:33:15 -0600

    Another test.

--- Xledger.Collections.Test/TestMemoryOwner.cs
+++ Xledger.Collections.Test/TestMemoryOwner.cs
@@ -1,9 +1,4 @@
-using System;
 using System.Buffers;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xledger.Collections.Memory;
 
 namespace Xledger.Collections.Test;
@@ -14,6 +9,12 @@ public class TestMemoryOwner {
         byte[] array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         IMemoryOwner<byte> memoryOwner = array.ToOwnedMemory();
         Assert.Equal(array, memoryOwner.Memory);
+    }
+
+    [Fact]
+    public void TestSlice() {
+        byte[] array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        IMemoryOwner<byte> memoryOwner = array.ToOwnedMemory();
         IMemoryOwner<byte> sliced = memoryOwner.Slice(3);
         Assert.Equal(array.AsMemory().Slice(3), sliced.Memory);
         IMemoryOwner<byte> sliced2 = sliced.Slice(1, 4);
