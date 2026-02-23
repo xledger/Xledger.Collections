73334283c1e8c46a470880533e90288cafe04d22
Matthew O'Connor <matthew.oconnor@xledger.com>
2026-02-23 11:41:13 -0700

    Test `new ImmArray` construction.

--- Xledger.Collections.Test/TestImmArray.cs
+++ Xledger.Collections.Test/TestImmArray.cs
@@ -235,6 +235,33 @@ public class TestImmArray {
         Assert.Equal(typeof(ImmArray<ExplicitIntArray>), c.GetType());
     }
 
+
+    [Fact]
+    public void TestNew() {
+        var wArr = new object[1] { "foo", };
+        var w = new ImmArray<object>(wArr);
+        Assert.Equal(typeof(ImmArray<object>), w.GetType());
+        Assert.Equal("foo", w[0]);
+
+        var x = new ImmArray<int>(new int[] { 1 });
+        Assert.Equal(typeof(ImmArray<int>), x.GetType());
+
+        var y = new ImmArray<int>(1);
+        Assert.Equal(typeof(ImmArray<int>), y.GetType());
+
+        var z =  new ImmArray<int>((ReadOnlySpan<int>)[1]);
+        Assert.Equal(typeof(ImmArray<int>), z.GetType());
+
+        var a = new ImmArray<int[]>(new int[] { 1 });
+        Assert.Equal(typeof(ImmArray<int[]>), a.GetType());
+
+        var b = new ImmArray<ImplicitIntArray>(new ImplicitIntArray(1));
+        Assert.Equal(typeof(ImmArray<ImplicitIntArray>), b.GetType());
+
+        var c = new ImmArray<ExplicitIntArray>(new ExplicitIntArray(1));
+        Assert.Equal(typeof(ImmArray<ExplicitIntArray>), c.GetType());
+    }
+
     public record ImplicitIntArray(int i) {
         public static implicit operator int[](ImplicitIntArray arr) => [arr.i];
     }
